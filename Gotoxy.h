b11af58e47b689713f4b26334e8db3d5b2ded40d
leonardo Nahin Elizalde Ramirez <38774885+lnerlam@users.noreply.github.com>
2020-03-15 20:21:43 -0600

    Add files via upload

---
+++ Gotoxy.h
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+ #include <windows.h>  
+ void gotoxy(int x,int y){  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);  
+ }  
+  
